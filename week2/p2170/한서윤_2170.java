24f8fc047f2bfbb51260924c717709a941dba36b
seoyun <seoyun@localhost>
2026-02-10 02:46:04 +0900

    [한서윤] 2주차 2170번 수정

--- week2/p2170/한서윤_2170.java
+++ week2/p2170/한서윤_2170.java
@@ -6,18 +6,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
-		List<int[]> list = new ArrayList<>();
+		int[][] list = new int[n][2]; // 2차원 배열로 변경.
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
-			list.add(new int[] { x, y });
+			list[i][0] = x;
+			list[i][1] = y;
 		}
-		list.sort((list1, list2) -> { // x 순으로 정렬.
+		Arrays.sort(list, (list1, list2) -> { // x 순으로 정렬.
 			if (list1[0] == list2[0])
 				return list1[1] - list2[1];
 			return list1[0] - list2[0];
 		});
-		int left = list.get(0)[0], right = list.get(0)[1], rst = 0;
+		int left = list[0][0], right = list[0][1], rst = 0;
 		for (int[] i : list) {
 			if (left <= i[0] && i[0] <= right) // 오른쪽으로 확장.
 				right = Math.max(right, i[1]);
